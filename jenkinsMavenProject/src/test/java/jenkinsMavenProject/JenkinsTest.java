783d37c26f030c86d1027ced36ac9771dff0c555
Pradeep Venkatesh <Pradeep Venkatesh@TYSS-Pradeep-Venkatesh>
2023-12-19 10:51:06 +0530

    first push

---
+++ jenkinsMavenProject/src/test/java/jenkinsMavenProject/JenkinsTest.java
@@ -0,0 +1,11 @@
+package jenkinsMavenProject;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+	@Test
+	public void demo() {
+		Reporter.log("JenkinsTest class executed...",true);
+	}
+}
