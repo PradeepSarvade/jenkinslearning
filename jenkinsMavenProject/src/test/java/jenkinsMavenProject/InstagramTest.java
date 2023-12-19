eecc71aaf939e2f060fec3e37e5d4c01c810e5cb
Pradeep Venkatesh <Pradeep Venkatesh@TYSS-Pradeep-Venkatesh>
2023-12-19 11:39:51 +0530

    2nd

--- jenkinsMavenProject/src/test/java/jenkinsMavenProject/InstagramTest.java
+++ jenkinsMavenProject/src/test/java/jenkinsMavenProject/InstagramTest.java
@@ -1,5 +1,16 @@
 package jenkinsMavenProject;
 
-public class InstagramTest {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+public class InstagramTest {
+@Test
+public void launch() {
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://demowebshop.tricentis.com/");
+	driver.findElement(By.xpath("//a[text()='Log in']"));
+}
 }
